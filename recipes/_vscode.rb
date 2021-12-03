e478dfef25a6aeef4e8401640ab896604dbfe668
byplayer <byplayer100@gmail.com>
2021-12-03 15:44:23 +0900

    🐛 fix issue

--- recipes/_vscode.rb
+++ recipes/_vscode.rb
@@ -1,6 +1,7 @@
 apt_repository 'vscode' do
   key ['https://packages.microsoft.com/keys/microsoft.asc']
-  components ['stab']
+  components ['main']
+  distribution 'stable'
   trusted true
   uri 'https://packages.microsoft.com/repos/vscode'
   action :add
